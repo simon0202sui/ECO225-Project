c7c0ff5be16e8a8aab342f78893576947bf0002e
PengyuSui <pengyu.sui@mail.utoronto.ca>
2024-01-30 17:07:41 -0500

    upload the dataset

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import pandas as pd
+
+csv_file_path = 'C:\\Users\\18097\\Desktop\\archive\\FastFoodRestaurants.csv'
+
+data = pd.read_csv(csv_file_path)
+
+print(data.head())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
